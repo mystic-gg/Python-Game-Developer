130d96e9a36d172bd86e058e8a911f56f03fd702
mystic-gg <ronanashwyn@gmail.com>
2026-03-11 16:53:37 +0000

    Create Rainbow-Circle.py
    Hi Jetlearn I hope you enjoy my new project! 😄

---
+++ Rainbow-Circle.py
@@ -0,0 +1,21 @@
+import pgzrun
+import random
+WIDTH = 500
+HEIGHT = 500
+def draw ():
+    screen.fill ("Black")
+    radius = 35
+    for i in range (25):
+     r = random.randint(1,255)
+     g = random.randint(1,255)
+     b = random.randint(1,255)
+     screen.draw.circle((250,250),radius,(r,g,b))
+     radius = radius + 9
+     
+     
+def update ():
+   pass
+ 
+        
+pgzrun.go()
+
